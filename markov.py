01fe8aa8c2e0ee01e9619c91dee4aec4168f6c8b
Jevon Morris <iheartbenzene@gmail.com>
2019-05-16 15:00:29 -0700

    initialize markov.py

--- markov.py
+++ markov.py
@@ -6,4 +6,11 @@ import random
 
 import wolframclient
 
-
+class Markov(object):
+    def __init__(self, order = 2, dictFile = '', max_words = 100):
+        self.table = {}
+        self.inputLineCount = 0
+        self.inputWordCount = 0
+        self.setOrder(order)
+        self.setMaxWords(max_words)
+        
