cafa510a3e8d4b3d555c6672780b98261544c2a5
Jevon Morris <iheartbenzene@gmail.com>
2019-05-17 17:11:16 -0700

    initialize character maps

--- text_generator.py
+++ text_generator.py
@@ -13,3 +13,12 @@ jekyll_hyde_file = "jekyll_and_hyde.txt"
 raw_text_alice = open(alice_file).read()
 raw_text_dracula = open(dracula_file).read()
 raw_text_jekyll = open(jekyll_hyde_file).read()
+
+raw_text_alice = raw_text_alice.lower()
+raw_text_dracula = raw_text_dracula.lower()
+raw_text_jekyll = raw_text_jekyll.lower()
+
+alice = sorted(list(set(raw_text_alice)))
+dracula = sorted(list(set(raw_text_dracula)))
+jekyll_hyde = sorted(list(raw_text_jekyll))
+
