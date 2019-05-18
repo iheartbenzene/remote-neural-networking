4473fda1e8c75f11a1f07367370ae7da3589c2c6
Jevon Morris <iheartbenzene@gmail.com>
2019-05-17 18:16:33 -0700

    build dictionary from alice in wonderland

--- ltsm_text_generator.py
+++ ltsm_text_generator.py
@@ -6,6 +6,9 @@ from keras.layers import LTSM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
+def char_to_int(text_list):
+    return dict((c, i) for i, c in enumerate(text_list))
+
 alice_file = "alice.txt"
 dracula_file = "dracula.txt"
 jekyll_hyde_file = "jekyll_and_hyde.txt"
@@ -18,7 +21,26 @@ raw_text_alice = raw_text_alice.lower()
 raw_text_dracula = raw_text_dracula.lower()
 raw_text_jekyll = raw_text_jekyll.lower()
 
+# Can be refactored into a function
 alice = sorted(list(set(raw_text_alice)))
+alice_to_int = char_to_int(alice)
 dracula = sorted(list(set(raw_text_dracula)))
+dracula_to_int = char_to_int(dracula)
 jekyll_hyde = sorted(list(raw_text_jekyll))
+jekyll_hyde_to_int = char_to_int(jekyll_hyde)
 
+# Can be refactored into a function
+alice_chars = len(raw_text_alice)
+alice_vocab = len(alice)
+dracula_chars = len(raw_text_dracula)
+dracula_vocab = len(dracula)
+jekyll_hyde_chars = len(raw_text_jekyll)
+jekyll_hyde_vocab = len(jekyll_hyde)
+
+sequence_length = 1000 #approximately 250 words?
+aliceX = []
+aliceY = []
+for i in range(0, alice_chars - sequence_length, 1):
+    sequence_in = raw_text_alice[i: i+sequence_length]
+    sequence_out = raw_text_alice[i + sequence_length]
+    aliceX.append([alice_to_int[char] for char in sequence_in])
